a98a9f136fc13396952ac3acd0ad5ed2b055db33
jxj <2039916844@qq.com>
2024-05-29 23:26:25 +0800

    xxj 的博客rss源

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,10 +2,10 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
+    title: 'Astro xxj | Blog',
+    description: 'xxj 的博客rss源',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
+    customData: `<language>en-zh</language>`,
   });
 }
